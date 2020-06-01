58b43e4ba9483040ef2f935a03a0c6c809bc7b11
Ethan <ethan.lv@feixitek.com>
2020-06-02 01:52:51 +0800

    change version info ,add new tag v0.6_test20200601

--- AutoVersion.py
+++ AutoVersion.py
@@ -3,68 +3,83 @@
 import commands,re,os,datetime
 #import Sundry as sy
 
-##############public function
-#get last tag
-def get_last_tag():
-    result=(commands.getoutput('git tag')).split()
-    if result:
-        return result[-1]
+def auto_version():
+    AutoVersion()
 
-def change_file_version(last_tag):
-
-    replace_version = "Version = '%s'" % last_tag
-    old_str = r"Version = [a-zA-Z0-9_.']*"
-
-    with open('Main.py','r') as f1,open('Main2.py','w') as f2:
-        for line in f1:
-            f2.write(re.sub(old_str,replace_version,line,1))
-    os.remove('Main.py')
-    os.rename('Main2.py','Main.py')
-
-def change_version_commit(last_tag):
-    commands.getoutput('git add *.py')
-    commands.getoutput('git commit -m "change version info ,add new tag %s" ' % last_tag)
-
-def time_now_tag():
-    return datetime.datetime.now().strftime('%Y%m%d_%H:%M:%S')
+def auto_cut_version(cut_type='test'):
+    AutoCutVersion(cut_type)
 
 
-######auto version
-def change_file_formal_version():
-    if get_last_tag():
-        change_file_version(get_last_tag())
+class Source(object):
+    def __init__(self):
+        pass
 
-def commit_main():
-    if get_last_tag():
-        change_version_commit(get_last_tag())
+    def get_last_tag(self):
+        result = (commands.getoutput('git tag')).split()
+        if result:
+            return result[-1]
 
-#####cut test version
-def get_cut_tag(cut_type):
-    if get_last_tag():
-        result=get_last_tag()+'_%s%s' % (cut_type,time_now_tag())
+    def get_cut_type_tag(self,last_tag,cut_type):
+        result = last_tag + '_%s%s' % (cut_type, self.time_now_tag())
         return result
 
-def change_file_cut_version(cut_type):
-    if get_last_tag():
-        change_file_version(get_cut_tag(cut_type))
+    #修改Main.py中的version变量
+    def change_file_version(self,last_tag):
+        replace_version = "Version = '%s'" % last_tag
+        old_str = r"Version = [a-zA-Z0-9_.']*"
+        with open('Main.py', 'r') as f1, open('Main2.py', 'w') as f2:
+            for line in f1:
+                f2.write(re.sub(old_str, replace_version, line, 1))
+        os.remove('Main.py')
+        os.rename('Main2.py', 'Main.py')
 
-def create_cut_tag(cut_type):
-    if get_last_tag():
-        commands.getoutput('git tag %s' % get_cut_tag(cut_type))
+    def change_version_commit(self,last_tag):
+        commands.getoutput('git add *.py')
+        commands.getoutput('git commit -m "change version info ,add new tag %s" ' % last_tag)
 
-def commit_main_cut_version(cut_type):
-    if get_last_tag():
-        change_version_commit(get_cut_tag(cut_type))
+    def create_tag_cut(self,last_tag):
+        commands.getoutput('git tag %s' % last_tag)
 
-#####call function
-def auto_version():
-    change_file_formal_version()
-    commit_main()
+    def time_now_tag(self):
+        return datetime.datetime.now().strftime('%Y%m%d_%H:%M:%S')
 
-def auto_cut_version(cut_type):
-    change_file_cut_version(cut_type)
-    commit_main_cut_version(cut_type)
-    create_cut_tag(cut_type)
+
+class AutoVersion(Source):
+    def __init__(self):
+        self.new_tag=self.get_last_tag()
+        self.auto_version()
+
+    def auto_version(self):
+        if self.new_tag:
+            self.change_file_version(self.new_tag)
+            self.change_version_commit(self.new_tag)
+        else:
+            print('Failed to get tag')
+
+
+class AutoCutVersion(Source):
+    def __init__(self,cut_type):
+        self.cut_type=cut_type
+        self.new_tag = self.get_last_tag()
+        self.auto_cut_version()
+
+    def auto_cut_version(self,cut_type):
+        if self.new_tag:
+            self.cut_tag = self.get_cut_tag()
+            self.change_file_version(self.cut_tag)
+            self.change_version_commit(self.cut_tag)
+            self.create_tag_cut(self.cut_tag)
+        else:
+            print('false')
+
+    def get_cut_tag(self):
+        if '_' in self.new_tag:
+            list_new_tag = self.new_tag.split('_')
+            result = self.get_cut_type_tag(list_new_tag[0],self.cut_type)
+        else:
+            result = self.get_cut_type_tag(self.new_tag,self.cut_type)
+        return result
+
 
 
 if __name__ == '__main__':
